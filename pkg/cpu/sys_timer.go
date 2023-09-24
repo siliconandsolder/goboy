a10afab95a7032ccaa11dc804b3ba07c0ec41559
Ben <ben.godin@hotmail.ca>
2023-09-24 00:07:42 -0400

    more timer testing

--- pkg/cpu/sys_timer.go
+++ pkg/cpu/sys_timer.go
@@ -40,60 +40,49 @@ func (timer *SysTimer) cycle() error {
 		return nil
 	}
 
+	timer.systemTimer++
 	timer.timaReload = false
 
-	//if timer.tac&4 == 4 { // TIMA enabled
-	//	if timer.timaTimer != getTimaInterval(timer.tac&3) {
-	//		timer.timaTimer++
-	//		return nil
-	//	}
-	//
-	//	timer.tima++
-	//	timer.timaTimer = 0
-	//
-	//	if timer.tima == 0 {
-	//		timer.bus.Write(bus.INTERRUPT_REQUEST, interrupts.TIMER)
-	//		timer.tima = timer.tma
-	//		timer.timaReload = true
-	//	}
-	//}
+	if timer.tac&4 == 4 { // TIMA enabled
+		timer.timaTimer++
+		if timer.timaTimer >= getTimaInterval(timer.tac&3) {
+			timer.tima++
+			timer.timaTimer -= getTimaInterval(timer.tac & 3)
 
-	if timer.cyclesToIrq > 0 {
-		timer.cyclesToIrq--
-		if timer.cyclesToIrq == 0 {
-			timer.bus.Write(bus.INTERRUPT_REQUEST, interrupts.TIMER)
-			timer.tima = timer.tma
-			timer.timaReload = true
+			if timer.tima == 0xFF {
+				timer.bus.Write(bus.INTERRUPT_REQUEST, interrupts.TIMER)
+				timer.tima = timer.tma
+				timer.timaReload = true
+			}
 		}
 	}
 
-	timer.systemTimerChange(timer.systemTimer + 1)
 	return nil
 }
 
 func (timer *SysTimer) write(addr uint16, val byte) {
 	switch addr {
 	case 0xFF04:
-		timer.systemTimerChange(0)
+		timer.timaTimer = 0
+		timer.systemTimer = 0
 		break
 	case 0xFF05:
-		if !timer.timaReload {
-			timer.tima = val
-		}
-		if timer.cyclesToIrq == 1 {
-			timer.cyclesToIrq = 0
-		}
+		//if !timer.timaReload {
+		//	timer.tima = val
+		//}
+		timer.tima = val
 		break
 	case 0xFF06:
-		if timer.timaReload {
-			timer.tima = val
-		}
+		//if timer.timaReload {
+		//	timer.tima = val
+		//}
 		timer.tma = val
 		break
 	case 0xFF07:
-		newBit := timer.lastBit & ((val & 4) >> 2)
-		timer.detectEdge(newBit)
-		timer.lastBit = newBit
+		if timer.tac&3 != val&3 {
+			timer.timaTimer = 0
+			timer.tima = timer.tma
+		}
 		timer.tac = val
 		break
 	}
@@ -119,39 +108,6 @@ func (timer *SysTimer) setStop(enabled bool) {
 		timer.systemTimer = 0
 	}
 	timer.stop = enabled
-}
-
-func (timer *SysTimer) detectEdge(newBit byte) {
-	if timer.lastBit == 1 && newBit == 0 {
-		timer.tima++
-		if timer.tima == 0 {
-			timer.cyclesToIrq = 1
-		}
-	}
-}
-
-func (timer *SysTimer) systemTimerChange(val uint16) {
-	timer.systemTimer = val
-
-	var newBit byte = 0
-	switch timer.tac & 3 {
-	case 0:
-		newBit = byte(timer.systemTimer >> 9 & 1)
-		break
-	case 3:
-		newBit = byte(timer.systemTimer >> 7 & 1)
-		break
-	case 2:
-		newBit = byte(timer.systemTimer >> 5 & 1)
-		break
-	case 1:
-		newBit = byte(timer.systemTimer >> 3 & 1)
-		break
-	}
-
-	newBit &= timer.tac & 4 >> 2
-	timer.detectEdge(newBit)
-	timer.lastBit = newBit
 }
 
 func getTimaInterval(code byte) uint16 {
